9cea0a49b4a134fbd063b46d7b8910f395290122
Alano Terblanche <alanoterblanche@gmail.com>
2018-10-21 01:45:49 +0200

    vehicles

--- vehicles.php
+++ vehicles.php
@@ -254,7 +254,8 @@ session_start();
         <script src="plugins/knob/jquery.knob.js" type="text/javascript"></script>
 
         <!-- datepicker -->
-        <script src="plugins/datepicker/bootstrap-datepicker.js" type="text/javascript"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/js/bootstrap-datepicker.js" type="text/javascript"></script>
+        <!--<script src="plugins/datepicker/bootstrap-datepicker.js" type="text/javascript"></script>-->
         <!-- daterangepicker -->
         <script src="plugins/daterangepicker/daterangepicker.js" type="text/javascript"></script>
 
