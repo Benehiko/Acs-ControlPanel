56d2c226a0692d8fcb8e242f2e562bc8c797c7f2
Alano Terblanche <alanoterblanche@gmail.com>
2018-10-21 01:21:15 +0200

    Fixed some import issues on vehicles

--- vehicles.php
+++ vehicles.php
@@ -236,9 +236,7 @@ session_start();
                 integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
                 crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-        
-        <!-- jQuery UI 1.11.2 -->
-        <script src="http://code.jquery.com/ui/1.11.2/jquery-ui.min.js" type="text/javascript"></script>
+
         <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
         <script>
             $.widget.bridge('uibutton', $.ui.button);
@@ -274,11 +272,10 @@ session_start();
         <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
         <script src="dist/js/pages/dashboard.js" type="text/javascript"></script>
         <!-- AdminLTE for demo purposes -->
-        <script src="dist/js/demo.js" type="text/javascript"></script>
+        <!--<script src="dist/js/demo.js" type="text/javascript"></script>-->
         <!-- JQuery UI -->
         <!--<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>-->
-        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-        <script src="http://trentrichardson.com/examples/timepicker/jquery-ui-timepicker-addon.js"></script>
+        <!--<script src="http://trentrichardson.com/examples/timepicker/jquery-ui-timepicker-addon.js"></script>-->
 
         <script src="jQuery/postData.js" type="text/javascript"></script>
         <script src="jQuery/checklogin.js" type="text/javascript"></script>
