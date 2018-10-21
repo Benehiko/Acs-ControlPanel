73947f73e3c7734d981ee1345ccad59430b690b1
Alano Terblanche <alanoterblanche@gmail.com>
2018-10-21 03:12:04 +0200

    Vehicles

--- vehicles.php
+++ vehicles.php
@@ -242,42 +242,28 @@ session_start();
                 crossorigin="anonymous"></script>
 
         <!-- Date Picker -->
-        <!--<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>-->
         <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
         <script type="text/javascript"
                 src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/js/bootstrap-datepicker.js"
                 type="text/javascript"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.6.3/jquery-ui-timepicker-addon.min.js"></script>
-        <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-        <script>
-            $.widget.bridge('uibutton', $.ui.button);
-        </script>
-        <!-- Bootstrap 3.3.2 JS -->
-        <!--<script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>-->
-
 
         <script src="https://momentjs.com/downloads/moment.js"></script>
+
         <!-- Morris.js charts -->
         <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
-        <!--<script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>-->
-        <!--<script src="plugins/morris/morris.min.js" type="text/javascript"></script>-->
 
         <!-- Sparkline -->
         <script src="plugins/sparkline/jquery.sparkline.min.js" type="text/javascript"></script>
+
         <!-- jvectormap -->
         <script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js" type="text/javascript"></script>
         <script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js" type="text/javascript"></script>
+
         <!-- jQuery Knob Chart -->
         <script src="plugins/knob/jquery.knob.js" type="text/javascript"></script>
-
-
-        <!--<script src="plugins/datepicker/bootstrap-datepicker.js" type="text/javascript"></script>-->
-        <!-- daterangepicker -->
-        <!--<script src="plugins/daterangepicker/daterangepicker.js" type="text/javascript"></script>-->
-
-        <!-- Bootstrap WYSIHTML5 -->
         <script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js" type="text/javascript"></script>
 
         <!-- iCheck -->
@@ -292,16 +278,18 @@ session_start();
         <script src="dist/js/pages/dashboard.js" type="text/javascript"></script>
         <!-- AdminLTE for demo purposes -->
         <!--<script src="dist/js/demo.js" type="text/javascript"></script>-->
+
         <!-- JQuery UI -->
-        <!--<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>-->
-
-
         <script src="jQuery/postData.js" type="text/javascript"></script>
         <script src="jQuery/checklogin.js" type="text/javascript"></script>
         <script src="jQuery/logout.js" type="text/javascript"></script>
         <script src="jQuery/filterVehicles.js" type="text/javascript"></script>
         <script src="jQuery/users.js" type="text/javascript"></script>
+
         <script>
+            <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+            $.widget.bridge('uibutton', $.ui.button);
+
             $("#vehicle-from").datetimepicker({
                 dateFormat: "yy-mm-dd",
                 timeFormat: "hh",
