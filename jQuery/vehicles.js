2fb11c7903e13e767bdd5a574e6e96e087d4d976
Alano Terblanche <alanoterblanche@gmail.com>
2018-10-21 04:27:53 +0200

    vehicles

--- jQuery/vehicles.js
+++ jQuery/vehicles.js
@@ -1,21 +1,18 @@
-$(function () {
-
-    $('#add-vehicle').on('click', function () {
-        e.preventDefault();
-        e.stopPropagation();
-        $.ajax({
-            type: 'POST',
-            url: '../phpscripts/addvehicle.php',
-            data: {
-                numberplate: $("#registration").val(),
-                username: $("#fleet-user").val()
-            },
-            success: function (d) {
-                alert("New Vehicle Added");
-            },
-            error: function () {
-                alert('Error adding vehicle');
-            }
-        });
+$('#add-vehicle').on('click', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $.ajax({
+        type: 'POST',
+        url: '../phpscripts/addvehicle.php',
+        data: {
+            numberplate: $("#registration").val(),
+            username: $("#fleet-user").val()
+        },
+        success: function (d) {
+            alert("New Vehicle Added");
+        },
+        error: function () {
+            alert('Error adding vehicle');
+        }
     });
 });
